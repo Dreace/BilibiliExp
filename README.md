413475cdb16008245cfb7f534fafc3acf7528491
Dreace <Dreace@Foxmail.com>
2019-12-01 19:08:55 +0800

    Version 1.1.2
    再次尝试解决兼容性问题

--- README.md
+++ README.md
@@ -3,11 +3,22 @@ B 站经验助手，自动投币视频、模拟移动端分享、经验获取统
 
 ![image-20191129204026268](https://s2.ax1x.com/2019/11/29/QEPjZq.png)
 
+# GitHub
+
+- 地址：[https://github.com/Dreace/BilibiliExp](https://github.com/Dreace/BilibiliExp)
+
 # 安装
 
 - Chrome 推荐使用扩展：「[Tampermonkey](https://chrome.google.com/webstore/detail/dhdgffkkebhmkfjojejmpbldmpobfkfo)」
 - Firefox 推荐使用扩展：「[Greasemonkey](https://addons.mozilla.org/en-US/firefox/addon/greasemonkey/)」
-- 脚本地址：https://greasyfork.org/scripts/393096-bilibiliexp
+- 脚本地址：[https://greasyfork.org/scripts/393096-bilibiliexp](https://greasyfork.org/scripts/393096-bilibiliexp)
+
+# 使用
+
+1. 使用「安装」中提到的任意一个插件安装脚本。
+2. 打开前缀为 https://www.bilibili.com/video/ 的视频播放页，此时插件会显示脚本正在运行。
+3. 等待 **10s** 左右会自动投币并在播放器下方显示经验情况。
+4. 如果在播放页脚本没有执行请到 [GitHub](https://github.com/Dreace/BilibiliExp) 反馈，并提供详细的控制台输出。
 
 # 功能
 
@@ -15,7 +26,7 @@ B 站经验助手，自动投币视频、模拟移动端分享、经验获取统
 
 - 自动投币（番剧一个，其他两个）
 - 模拟移动端分享（测试功能）
-- 统计今日已获取经验
+- 统计今日已获取经验（番剧页面暂未支持）
 - 估计升级剩余时间（按每天 65 经验计算）
 
 ## 开发中（可能会有）
--- main.js
+++ main.js
@@ -16,207 +16,206 @@
 // @require     https://static.hdslb.com/js/md5.js
 // ==/UserScript==
 // file:///C:/WorkSpace/JavaScript/BilibiliExp/main.js
-(function () {
-    'use strict';
-    if (location.href.match('link.acg.tv/forum.php') && location.href.match('access_key') && window.opener) {
-        window.stop();
-        document.children[0].innerHTML = '<title>BilibiliExp - 获取 Access Key</title><meta charset="UTF-8" name="viewport" content="width=device-width">正在跳转……';
-        window.opener.postMessage('get_access_key: ' + location.href, '*');
-        return;
-    }
-    var coinUrl = "https://api.bilibili.com/x/web-interface/nav?build=0&mobi_app=web";
-    var addCoinUrl = "https://api.bilibili.com/x/web-interface/coin/add";
-    var shareUrl = "https://app.bilibili.com/x/v2/view/share/complete";
-    var shareUrlPre = "https://app.bilibili.com/x/v2/view/share/click";
-    var rewardUrl = "https://account.bilibili.com/home/reward";
-    var totalCoin = 0;
-    var expToday = 0;
-    var aid = "";
-    var bili_jct = getCookie("bili_jct");
-    try {
-        aid = unsafeWindow.vd ? unsafeWindow.vd.aid : unsafeWindow.aid;
-    } catch (error) {
-        console.error("[BilibiliExp] aid 获取失败");
-        return;
-    }
-    var access_key = GM_getValue('access_key');
-    if (access_key) {
-        checkKeyStatus(access_key);
-    } else {
-        getKey();
-    }
-    if (aid) {
-        gmAjax({
-            url: rewardUrl,
-            method: 'GET'
-        }).then(function (res) {
-            if (res.code == 0) {
-                expToday = 50 - res.data.coins_av;
-                if (!res.data.share_av && access_key) {
-                    var shareData = {
-                        access_key: access_key.key,
-                        actionKey: "appkey",
-                        aid: aid,
-                        build: "8960",
-                        device: "phone",
-                        epid: "",
-                        from: "711",
-                        mobi_app: "iphone",
-                        platform: "ios",
-                        season_id: "",
-                        share_channel: "qq",
-                        share_trace_id: hex_md5(new Date()),
-                        statistics: "%7B%22appId%22%3A1%2C%22version%22%3A%225.50.1%22%2C%22abtest%22%3A%22890%22%2C%22platform%22%3A1%7D"
-                    };
-                    var signed = get_sign(shareData, "c2ed53a74eeefe3cf99fbd01d8c9c375");
-                    gmAjax({
-                        method: "POST",
-                        url: shareUrlPre,
-                        data: signed.data + "&sign=" + signed.sign,
-                        headers: { "Content-Type": "application/x-www-form-urlencoded" }
-                    }).then(function (res) {
-                        if (res.code == 0) {
-                            return gmAjax({
-                                method: "POST",
-                                url: shareUrl,
-                                data: signed.data + "&sign=" + signed.sign,
-                                headers: { "Content-Type": "application/x-www-form-urlencoded" }
-                            });
-                        }
-                    }).then(function (res) {
-                        console.log("[BilibiliExp] " + res.data.toast);
-                    });
-                }
-            } else {
-                console.error("[BilibiliExp] 等级信息获取失败");
-            }
-        }).then(function () {
-            return biliAjax({
-                url: coinUrl,
-                type: 'GET',
-                dataType: 'json'
-            });
-        }).then(function (res) {
-            totalCoin = res.data.money;
-            console.log("[BilibiliExp] 当前硬币 " + totalCoin + " 个");
-            if (totalCoin < 50) {
-                console.log("[BilibiliExp] 硬币小于 50,暂不投币");
-            } else {
-                if (expToday == 0) {
-                    console.log("[BilibiliExp] 今日已获取全部经验");
-                }
-                return new Promise(function (resolve, reject) {
-                    setTimeout(function () {
-                        resolve();
-                    }, 10000);
+
+'use strict';
+if (location.href.match('link.acg.tv/forum.php') && location.href.match('access_key') && window.opener) {
+    window.stop();
+    document.children[0].innerHTML = '<title>BilibiliExp - 获取 Access Key</title><meta charset="UTF-8" name="viewport" content="width=device-width">正在跳转……';
+    window.opener.postMessage('get_access_key: ' + location.href, '*');
+    return;
+}
+var coinUrl = "https://api.bilibili.com/x/web-interface/nav?build=0&mobi_app=web";
+var addCoinUrl = "https://api.bilibili.com/x/web-interface/coin/add";
+var shareUrl = "https://app.bilibili.com/x/v2/view/share/complete";
+var shareUrlPre = "https://app.bilibili.com/x/v2/view/share/click";
+var rewardUrl = "https://account.bilibili.com/home/reward";
+var totalCoin = 0;
+var expToday = 0;
+var aid = "";
+var bili_jct = getCookie("bili_jct");
+try {
+    aid = unsafeWindow.vd ? unsafeWindow.vd.aid : unsafeWindow.aid;
+} catch (error) {
+    console.error("[BilibiliExp] aid 获取失败");
+    return;
+}
+var access_key = GM_getValue('access_key');
+if (access_key) {
+    checkKeyStatus(access_key);
+} else {
+    getKey();
+}
+if (aid) {
+    gmAjax({
+        url: rewardUrl,
+        method: 'GET'
+    }).then(function (res) {
+        if (res.code == 0) {
+            expToday = 50 - res.data.coins_av;
+            if (!res.data.share_av && access_key) {
+                var shareData = {
+                    access_key: access_key.key,
+                    actionKey: "appkey",
+                    aid: aid,
+                    build: "8960",
+                    device: "phone",
+                    epid: "",
+                    from: "711",
+                    mobi_app: "iphone",
+                    platform: "ios",
+                    season_id: "",
+                    share_channel: "qq",
+                    share_trace_id: hex_md5(new Date()),
+                    statistics: "%7B%22appId%22%3A1%2C%22version%22%3A%225.50.1%22%2C%22abtest%22%3A%22890%22%2C%22platform%22%3A1%7D"
+                };
+                var signed = get_sign(shareData, "c2ed53a74eeefe3cf99fbd01d8c9c375");
+                gmAjax({
+                    method: "POST",
+                    url: shareUrlPre,
+                    data: signed.data + "&sign=" + signed.sign,
+                    headers: { "Content-Type": "application/x-www-form-urlencoded" }
+                }).then(function (res) {
+                    if (res.code == 0) {
+                        return gmAjax({
+                            method: "POST",
+                            url: shareUrl,
+                            data: signed.data + "&sign=" + signed.sign,
+                            headers: { "Content-Type": "application/x-www-form-urlencoded" }
+                        });
+                    }
+                }).then(function (res) {
+                    console.log("[BilibiliExp] " + res.data.toast);
                 });
             }
-        }).then(function () {
-            if (totalCoin >= 50 && expToday > 0) {
-                console.log("[BilibiliExp] 准备投币");
+        } else {
+            console.error("[BilibiliExp] 等级信息获取失败");
+        }
+    }).then(function () {
+        return biliAjax({
+            url: coinUrl,
+            type: 'GET',
+            dataType: 'json'
+        });
+    }).then(function (res) {
+        totalCoin = res.data.money;
+        console.log("[BilibiliExp] 当前硬币 " + totalCoin + " 个");
+        if (totalCoin < 50) {
+            console.log("[BilibiliExp] 硬币小于 50,暂不投币");
+        } else {
+            if (expToday == 0) {
+                console.log("[BilibiliExp] 今日已获取全部经验");
+            }
+            return new Promise(function (resolve, reject) {
+                setTimeout(function () {
+                    resolve();
+                }, 10000);
+            });
+        }
+    }).then(function () {
+        if (totalCoin >= 50 && expToday > 0) {
+            console.log("[BilibiliExp] 准备投币");
+            return addCoin();
+        }
+    }).then(function (res) {
+        if (res && res.code == 0) {
+            console.log("[BilibiliExp] 投了一个币");
+            expToday -= 10;
+            if (expToday > 0) {
                 return addCoin();
             }
-        }).then(function (res) {
-            if (res && res.code == 0) {
-                console.log("[BilibiliExp] 投了一个币");
-                expToday -= 10;
-                if (expToday > 0) {
-                    return addCoin();
-                }
-            }
-        }).then(function (res) {
-            if (res && res.code == 0) {
-                console.log("[BilibiliExp] 又投了一个币");
-            }
-            return gmAjax({
-                url: rewardUrl,
-                method: 'GET'
+        }
+    }).then(function (res) {
+        if (res && res.code == 0) {
+            console.log("[BilibiliExp] 又投了一个币");
+        }
+        return gmAjax({
+            url: rewardUrl,
+            method: 'GET'
+        });
+    }).then(function (res) {
+        if (res.code == 0) {
+            var rewardInfo = res.data;
+            var link = document.createElement('link');
+            link.rel = 'stylesheet';
+            link.href = "//at.alicdn.com/t/font_1537779_4srood2g1uk.css";
+            document.body.appendChild(link);
+            var style = "color: inherit;display: inline-block;line-height: 1;";
+            var spansData = [];
+            var total = 0;
+            total += rewardInfo.login ? 5 : 0;
+            spansData.push({
+                ok: rewardInfo.login,
+                name: "每日登录",
+                text: ((rewardInfo.login ? 5 : 0) + "/5"),
+                className: "icon-login"
             });
-        }).then(function (res) {
-            if (res.code == 0) {
-                var rewardInfo = res.data;
-                var link = document.createElement('link');
-                link.rel = 'stylesheet';
-                link.href = "//at.alicdn.com/t/font_1537779_4srood2g1uk.css";
-                document.body.appendChild(link);
-                var style = "color: inherit;display: inline-block;line-height: 1;";
-                var spansData = [];
-                var total = 0;
-                total += rewardInfo.login ? 5 : 0;
-                spansData.push({
-                    ok: rewardInfo.login,
-                    name: "每日登录",
-                    text: ((rewardInfo.login ? 5 : 0) + "/5"),
-                    className: "icon-login"
-                });
-                total += rewardInfo.share_av ? 5 : 0;
-                spansData.push({
-                    ok: rewardInfo.share_av,
-                    name: "分享视频",
-                    text: ((rewardInfo.share_av ? 5 : 0) + "/5"),
-                    className: "icon-share"
-                });
-                total += rewardInfo.watch_av ? 5 : 0;
-                spansData.push({
-                    ok: rewardInfo.watch_av,
-                    name: "观看视频",
-                    text: ((rewardInfo.watch_av ? 5 : 0) + "/5"),
-                    className: "icon-play"
-                });
-                total += rewardInfo.coins_av;
-                spansData.push({
-                    ok: rewardInfo.coins_av == 50,
-                    name: "视频投币",
-                    text: (rewardInfo.coins_av + "/50"),
-                    className: "icon-coin"
-                });
-                spansData.push({
-                    ok: total == 65,
-                    name: "总计",
-                    text: (total + "/65"),
-                    className: "icon-total"
-                });
-                spansData.push({
-                    ok: false,
-                    name: ("最快到 " + (rewardInfo.level_info.current_level + 1) + " 级剩余天数"),
-                    text: (Math.ceil((rewardInfo.level_info.next_exp - rewardInfo.level_info.current_exp) / 65) + " 天"),
-                    className: "icon-day"
-                });
-                var bar = document.getElementById("arc_toolbar_report");
-                bar.style.height = "60px";
-                var ops = document.createElement('div');
-                ops.className = "ops";
-                spansData.forEach(function (item) {
-                    var span = document.createElement("span");
-                    if (item.ok) {
-                        span.style = "color:rgb(251, 114, 153);";
-                    } else {
-                        span.style = "color:rgb(80, 80, 80);";
-                    }
-                    span.title = item.name;
-                    span.innerHTML = ("<i class=\"" + item.className + " iconfont\" style=\"" + style + "\"></i>" + item.text);
-                    ops.appendChild(span.cloneNode(true));
-                });
-                ops.style.marginTop = "10px";
-                bar.appendChild(ops);
-            }
-        });
-    }
-    function addCoin() {
-        return biliAjax({
-            url: addCoinUrl,
-            type: 'POST',
-            dataType: 'json',
-            data: {
-                aid: aid,
-                multiply: "1",
-                select_like: 0,
-                cross_domain: true,
-                csrf: bili_jct
-            }
-        });
-    }
-})();
+            total += rewardInfo.share_av ? 5 : 0;
+            spansData.push({
+                ok: rewardInfo.share_av,
+                name: "分享视频",
+                text: ((rewardInfo.share_av ? 5 : 0) + "/5"),
+                className: "icon-share"
+            });
+            total += rewardInfo.watch_av ? 5 : 0;
+            spansData.push({
+                ok: rewardInfo.watch_av,
+                name: "观看视频",
+                text: ((rewardInfo.watch_av ? 5 : 0) + "/5"),
+                className: "icon-play"
+            });
+            total += rewardInfo.coins_av;
+            spansData.push({
+                ok: rewardInfo.coins_av == 50,
+                name: "视频投币",
+                text: (rewardInfo.coins_av + "/50"),
+                className: "icon-coin"
+            });
+            spansData.push({
+                ok: total == 65,
+                name: "总计",
+                text: (total + "/65"),
+                className: "icon-total"
+            });
+            spansData.push({
+                ok: false,
+                name: ("最快到 " + (rewardInfo.level_info.current_level + 1) + " 级剩余天数"),
+                text: (Math.ceil((rewardInfo.level_info.next_exp - rewardInfo.level_info.current_exp) / 65) + " 天"),
+                className: "icon-day"
+            });
+            var bar = document.getElementById("arc_toolbar_report");
+            bar.style.height = "60px";
+            var ops = document.createElement('div');
+            ops.className = "ops";
+            spansData.forEach(function (item) {
+                var span = document.createElement("span");
+                if (item.ok) {
+                    span.style = "color:rgb(251, 114, 153);";
+                } else {
+                    span.style = "color:rgb(80, 80, 80);";
+                }
+                span.title = item.name;
+                span.innerHTML = ("<i class=\"" + item.className + " iconfont\" style=\"" + style + "\"></i>" + item.text);
+                ops.appendChild(span.cloneNode(true));
+            });
+            ops.style.marginTop = "10px";
+            bar.appendChild(ops);
+        }
+    });
+}
+function addCoin() {
+    return biliAjax({
+        url: addCoinUrl,
+        type: 'POST',
+        dataType: 'json',
+        data: {
+            aid: aid,
+            multiply: "1",
+            select_like: 0,
+            cross_domain: true,
+            csrf: bili_jct
+        }
+    });
+}
 function gmAjax(opt) {
     return new Promise(function (resolve, reject) {
         GM.xmlHttpRequest({
